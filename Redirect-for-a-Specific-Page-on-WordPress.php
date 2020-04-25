b6db0b6c71fe3d10558aecd1b16631e8a06a34eb
Kimberley Griffith <kimberley.m.griffith@gmail.com>
2020-04-25 10:08:51 -0400

    Add files via upload

--- Redirect-for-a-Specific-Page-on-WordPress.php
+++ Redirect-for-a-Specific-Page-on-WordPress.php
@@ -1,14 +1,18 @@
 <?php
 
 //add to the functions.php file
+//this snippet creates sidebar-1
 
 function redirect_to_another_page() {
-$page_id_to_redirect = 119; //enter page id here
-$destination = 'https://kimberleygriffith.com'; //enter destination url here
-if ( is_page($page_id_to_redirect) ) {
-D0d_redirect( $destination, 301 );
-exit;
-}
+	
+	$page_id_to_redirect = 119; //enter page id here
+	$destination = 'https://kimberleygriffith.com'; //enter destination url here
+	
+	if ( is_page($page_id_to_redirect) ) {
+		wp_redirect( $destination, 301 );
+		exit;
+	}
+	
 }
 add_action( 'template_redirect', 'redirect_to_another_page' );
 
